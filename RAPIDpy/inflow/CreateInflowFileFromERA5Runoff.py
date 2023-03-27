154babb034e76ab5116938ede7959ca089abac0d
Dr Riley Hales <39097632+rileyhales@users.noreply.github.com>
2023-03-27 16:42:25 -0600

    change 4 typo to 5

--- RAPIDpy/inflow/CreateInflowFileFromERA5Runoff.py
+++ RAPIDpy/inflow/CreateInflowFileFromERA5Runoff.py
@@ -61,7 +61,7 @@ class CreateInflowFileFromERA5Runoff(CreateInflowFileFromGriddedRunoff):
             self.runoff_vars = [self.vars_oi[3][-1]]
         elif nc_vars == self.vars_oi[4]:
             self.runoff_vars = [self.vars_oi[4][-1]]
-        elif nc_vars == self.vars_oi[4]:
+        elif nc_vars == self.vars_oi[5]:
             self.runoff_vars = [self.vars_oi[5][2]]
 
         else:
